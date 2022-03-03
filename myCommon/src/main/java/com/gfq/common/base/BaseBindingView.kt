cf2056eb9eb8686d3f7a2a0735c8c9306f584362
GaoFuQiang <1164842895@qq.com>
2022-03-03 14:07:49 +0800

    .

---
+++ myCommon/src/main/java/com/gfq/common/base/BaseBindingView.kt
@@ -0,0 +1,32 @@
+package com.gfq.common.base
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+/**
+ *  2022/3/3 10:36
+ * @auth gaofuq
+ * @description
+ */
+abstract class BaseBindingView<binding : ViewDataBinding>(
+    context: Context,
+    attrs: AttributeSet?,
+) : FrameLayout(context, attrs) {
+
+    abstract fun layoutResId(): Int
+
+   protected val vBinding by lazy {
+        DataBindingUtil.inflate<binding>(
+            LayoutInflater.from(context),
+            layoutResId(),
+            null,
+            false
+        ).apply {
+            addView(root)
+        }
+    }
+}
